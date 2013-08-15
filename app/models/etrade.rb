3221d36330e2461910b2488bab34f9c94e46c0e4
MRoytman <mikeroytman@gmail.com>
2013-08-14 23:42:59 -0500

    start ETrade model
    oauth or static tokens needs to be resolved

--- app/models/etrade.rb
+++ app/models/etrade.rb
@@ -1,36 +1,58 @@
-require 'dwolla'
+require 'rubygems'
+require 'oauth'
+require 'net/http'
+require 'httparty'
+require 'nokogiri'
+#screen scrape or oauth? decisions, decisions.
+#TODO: revisit gems to check for dependencies. I just open up a pandora's box of security flaws.
 
 class ETrade
-  # for one time donation
-  def donate(amount, charity_group_id)
-    raise PaymentAccountInvalid if !self.valid?
-    raise CharityGroupInvalid if !(charity = CharityGroup.find(charity_group_id))
-    raise AmountInvalid if amount.blank? || amount < 0
+  include HTTParty
+  base_uri 'https://etws.etrade.com/'
+  format :xml
 
-    Dwolla::token = token
-    transaction_id = Dwolla::Transactions.send({:destinationId => App.dwolla['account_id'],
-                                                :amount => amount.to_f,
-                                                :pin => pin})
+  #TODO:
+  CONSUMER_TOKEN = {
+    :token => \access_token.token\,
+    :secret => \access_token.secret\
+  }
 
-    # amount = amount after processing fee(s)
-    donation = donor.donations.build(:amount => amount,
-                                     :charity_group_id => charity.id,
-                                     :transaction_id => transaction_id,
-                                     :transaction_processor => processor)
-    donation.save(false)
-    donation
-  end
-private
+  ACCESS_TOKEN = {
+    :token => \access_token.token\,
+    :secret => \access_token.secret\
+  }
 
-  def set_requires_reauth
-    # just being nice for create
-    self.requires_reauth = false if !self.requires_reauth
-    true
+  #For y'alls error checkings
+  def self.test_connection
+
   end
 
-  def downcase_processor
-    # just being nice for create
-    self.processor = self.processor.downcase if self.processor
-    true
+  def self.authorize(oauth_consumer_key=CONSUMER_KEY, oauth_token=ACCESS_TOKEN)
+    get("https://us.etrade.com/e/etws/authorize?key=#{oauth_consumer_key}&token=#{oauth_token}")
   end
+
+  #Accounts API
+  def self.get_accounts
+    #If we have multiple accounts y'all gotta let me know
+    doc = Nokogiri::XML(get("/accounts/rest/accountlist"))
+  end
+
+  def self.get_net_account_value
+    doc = ETrade.get_accounts
+    return doc.xpath("//AccountListResponse//Account//netAccountValue").inner_text
+  end
+
+  def self.method_missing(method_id, *args)
+    if match = /get_products_by_([_a-zA-Z]\w*)/.match(method_id.to_s)
+      attribute_names = match.captures.last.split('_and_')
+
+      request = ""
+      attribute_names.each_with_index { |name, idx| request = request + name + "=" + args[idx] + (attribute_names.length-1 == idx ? "" : "&") }
+
+      get_products(request)
+    else
+      super
+    end
+  end
+
 end
