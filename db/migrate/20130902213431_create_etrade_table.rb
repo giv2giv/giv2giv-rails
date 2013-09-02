0274efca5a3a5d4b5a003bfc4dc95971514f06aa
MRoytman <mikeroytman@gmail.com>
2013-09-02 16:37:44 -0500

    create change in migration for fees and balance

--- db/migrate/20130902213431_create_etrade_table.rb
+++ db/migrate/20130902213431_create_etrade_table.rb
@@ -1,7 +1,9 @@
 class CreateEtradeTable < ActiveRecord::Migration
-  def up
-  end
-
-  def down
+  def change
+    create_table(:etrade) do |t|
+      t.column :date, :datetime
+      t.column :balance, :float
+      t.column :cumulative_fees, :float
+    end
   end
 end
