11aff3c0d566e574b05b1b5e4641086ed294143c
MRoytman <mikeroytman@gmail.com>
2013-09-02 16:45:42 -0500

    update migration to not allow null on balance

--- db/migrate/20130902213431_create_etrade_table.rb
+++ db/migrate/20130902213431_create_etrade_table.rb
@@ -1,9 +1,10 @@
 class CreateEtradeTable < ActiveRecord::Migration
   def change
-    create_table(:etrade) do |t|
-      t.column :date, :datetime
-      t.column :balance, :float
-      t.column :cumulative_fees, :float
+    create_table :etrade do |t|
+      t.timestamps
+      t.datetime :date
+      t.float :balance, null: false
+      t.float :fees
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130902162932) do
+ActiveRecord::Schema.define(:version => 20130902213431) do
 
   create_table "charities", :force => true do |t|
     t.string   "name",                :null => false
@@ -82,6 +82,12 @@ ActiveRecord::Schema.define(:version => 20130902162932) do
     t.string   "phone_number"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+  end
+
+  create_table "etrade", :force => true do |t|
+    t.datetime "date"
+    t.float    "balance"
+    t.float    "cumulative_fees"
   end
 
   create_table "payment_accounts", :force => true do |t|
