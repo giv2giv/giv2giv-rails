cbc9de76d0cae1460db4fb26837d9660a9fee054
MRoytman <mikeroytman@gmail.com>
2013-09-12 22:24:14 -0500

    current price and total shares calcuation

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,8 +1,18 @@
+require 'bigdecimal'
 
 class Share < ActiveRecord::Base
   validates :donor_id, :presence => true
   validates :charity_group_id, :presence => true
 
+  def current_total_price
+  #returns bigdecimal
+    return Share.select(:count).map{|c| c.count.to_d}.sum
+  end
+
+  def current_price
+    current_etrade_total = Etrade.get_net_account_value
+    return current_etrade_total/Share.current_etrade_total.to_f
+  end
 
 end
 
