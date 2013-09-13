af44c24f36c48606f93afc35dec7c180105bde34
MRoytman <mikeroytman@gmail.com>
2013-09-12 21:49:42 -0500

    shares: migrate up!

--- db/migrate/20130913024640_sharestable.rb
+++ db/migrate/20130913024640_sharestable.rb
@@ -1,7 +1,11 @@
-class Sharestable < ActiveRecord::Migration
-  def up
-  end
-
-  def down
+class CreateSharesTable < ActiveRecord::Migration
+  def change
+    create_table :shares do |t|
+      t.timestamps
+      t.integer :donor_id, null: false
+      t.integer :charity_group_id, null: false
+      t.float :count, null: false
+      t.float :price_at_issue
+    end
   end
 end
