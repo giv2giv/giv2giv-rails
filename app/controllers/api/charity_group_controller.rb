5be580b6307b40483f69de50faaae3a5f7c9e71a
elgalesmana41 <elga@41studio.com>
2013-09-23 09:53:48 +0700

    close issue#10

--- app/controllers/api/charity_group_controller.rb
+++ app/controllers/api/charity_group_controller.rb
@@ -5,8 +5,8 @@ class Api::CharityGroupController < Api::BaseController
 
   def index
     page = params[:page] || 1
-    per_page = [params[:per_page], 30].compact.min # limit to 30 results per page
-    results = CharityGroup.all.paginate(:per_page => per_page, :page => page)
+    perpage = [params[:per_page], 30].compact.min
+    results = CharityGroup.find(:all).paginate(:page => page, :per_page => perpage)
 
     respond_to do |format|
       format.json { render json: results }
