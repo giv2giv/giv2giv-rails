b15985d536834093e1bb1a629db1c2c8e6b5bf23
MRoytman <mikeroytman@gmail.com>
2013-11-12 20:10:01 -0600

    active_record_all_the_things

--- db/migrate/20130902162932_add_tags.rb
+++ db/migrate/20130902162932_add_tags.rb
@@ -1,18 +1,6 @@
 class AddTags < ActiveRecord::Migration
-
-
-  def self.up
-    execute 'ALTER TABLE tags ENGINE = MyISAM'
-    execute 'CREATE FULLTEXT INDEX fulltext_tags ON tags(name)'
-  end
-
-  def self.down
-    execute 'ALTER TABLE tags ENGINE = InnoDB'
-    execute 'DROP INDEX fulltext_tags ON tags'
-  end
-
   def change
-    create_table :tags do |t|
+    create_table(:tags, options: 'ENGINE = MyISAM') do |t|
       t.integer :id, :null => false
       t.text :name
       t.timestamps
@@ -23,6 +11,7 @@ class AddTags < ActiveRecord::Migration
       t.references :tag, :null => false
     end
 
+    add_index(:tags, :name, type: :fulltext)
     add_index(:charities_tags, [:charity_id, :tag_id], :unique => true)
     add_index :tags, :id
   end
