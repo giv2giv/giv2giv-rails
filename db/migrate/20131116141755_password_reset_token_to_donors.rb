f7bde3be2106d11bc0c7223eeb5b2a8f66286712
Elga <elga@41studio.com>
2013-11-16 23:12:49 +0700

    fix migration #76

--- db/migrate/20131116141755_password_reset_token_to_donors.rb
+++ db/migrate/20131116141755_password_reset_token_to_donors.rb
@@ -1,4 +1,4 @@
-class AddPasswordResetTokenToDonors < ActiveRecord::Migration
+class PasswordResetTokenToDonors < ActiveRecord::Migration
   def change
     add_column :donors, :password_reset_token, :string
     add_column :donors, :expire_password_reset, :datetime
