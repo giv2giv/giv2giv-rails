426e1f1fbcea3fa88fbf5ec5780444f7744bbceb
Michael Blinn <michael.blinn@giv2giv.org>
2013-12-10 00:01:51 -0500

    Correctly applying donations to endowments

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -7,40 +7,27 @@ class Donation < ActiveRecord::Base
     donation.validates :gross_amount
   end
 
-  PER_SHARE_DEFAULT = 100000.0
-  STRIPE_FEES = App.stripe["fees"]
-  STRIPE_FEES_CENTS = App.stripe["fees_cents"]
-
   class << self
     
-    def add_donation(gross_amount, subscription_id)
+    def add_donation(subscription_id, gross_amount, transaction_fees, net_amount)
       
-      check_share_price = Share.last
-      if check_share_price.blank?
-        per_share = PER_SHARE_DEFAULT
-      else
-        per_share = check_share_price.donation_price
-      end
-
-      net_amount = (gross_amount - (gross_amount * STRIPE_FEES)) - STRIPE_FEES_CENTS
-      transaction_fee = gross_amount - net_amount
+      share_price = Share.last.donation_price
 
       donor_subscription = DonorSubscription.find_by_stripe_subscription_id(subscription_id)
 
-      buy_shares = (BigDecimal("#{net_amount}") / BigDecimal("#{per_share}"))
+      buy_shares = (BigDecimal("#{net_amount}") / BigDecimal("#{share_price}"))
       donation = Donation.new(
-                             :gross_amount => gross_amount,
+                             :donor_id => donor_subscription.donor_id,
                              :endowment_id => donor_subscription.endowment_id,
                              :payment_account_id => donor_subscription.payment_account_id,
-                             :shares_added => buy_shares,
-                             :donor_id => donor_subscription.donor_id,
-                             :transaction_fees => transaction_fee,
+                             :gross_amount => gross_amount,
+                             :transaction_fees => transaction_fees,
                              :net_amount => net_amount
+                             :shares_added => buy_shares,
                              )
 
       if donation.save
-        donor = Donor.find(donor_subscription.donor_id)
-        {"gross_amount" => gross_amount, "email" => donor.email}
+        {"gross_amount" => gross_amount, "status" => "success"}
       else
         puts "ERROR!"
       end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,16 @@
 Stripe.api_key = App.stripe["api_key"]
 
+STRIPE_FEES = App.stripe["fees"]
+STRIPE_FEES_CENTS = App.stripe["fees_cents"]
+
 StripeEvent.setup do
+
   subscribe 'charge.succeeded' do |event|
+
+    def net_amount (gross_amount)
+      net_amount = (((gross_amount - (gross_amount * STRIPE_FEES)) - STRIPE_FEES_CENTS).to_f * 10).ceil / 10.0
+    end
+
     invoice = event.data.object.invoice
     
     if invoice.blank?    
@@ -12,25 +21,43 @@ StripeEvent.setup do
 
     if ret_invoice.blank?
       # one-time-payment
-      stripe_amount = event.data.object.amount / 100
-      Donation.add_donation(stripe_amount, event.data.object.id)
+      charge_amount = event.data.object.amount / 100
+      donation_amount = net_amount(charge_amount)
+      transaction_fees = charge_amount - donation_amount
+
+      subscription = DonorSubscription.find_by_stripe_subscription_id(event.data.object.id) # will this work? stripe charge.id
+      Donation.add_donation(subscription.id, charge_amount, transaction_fees, donation_amount)
+
+      if donation.save
+        donor = Donor.find(subscriptions.first.donor_id)
+        DonorMailer.charge_success(donor.email, charge_amount).deliver
+      else
+        puts "ERROR!"
+      end
+
     else
-      # donor subscriptions
-      # ret_invoice.lines.data = donor.subscriptions.each 
-      total_gross_amount = 0
-      email = ""
-      ret_invoice.lines.data.each do |line_data|
-        # stripe_amount = subscription.amount 
-        stripe_amount = line_data.amount / 100
-        # parsing data subscriptions from stripe , for make the subscription is active
-        # donor.subscriptions.each do |subscription|  # donor is the donor who created this charge
-          donation = Donation.add_donation(stripe_amount, line_data.id)  # each charity_id gets its correct donation
-          total_gross_amount += donation["gross_amount"]
-          email = donation["email"]
-        # end
-      end # invoice lines data
-      DonorMailer.charge_success(email, total_gross_amount).deliver
-    end
-    
-  end # end charge.successed
+      # recurring subscriptions
+      charge_amount = ret_invoice.lines.data.amount / 100
+      donation_amount = net_amount(charge_amount)
+      transaction_fees = charge_amount - donation_amount
+
+      subscriptions = DonorSubscription.find_by_stripe_subscription_id(ret_invoice.lines.data.id) # will this work? stripe subscription.id
+      subscriptions_gross_amount_sum = donor_subscriptions.sum(gross_amount)
+
+      subscriptions.each do |subscription|
+         this_endowment_percentage_of_gross = subscription.gross_amount / subscriptions_gross_amount_sum # What % is this endowment to all subscribed endowments?
+         this_endowment_portion_of_net = this_endowment_percentage_of_gross * donation_amount # apply same % to charge net fees
+         Donation.add_donation(subscription.id, charge_amount, transaction_fees, donation_amount)
+      end
+
+      if donation.save
+        donor = Donor.find(subscriptions.first.donor_id)
+        DonorMailer.charge_success(donor.email, charge_amount).deliver
+      else
+        puts "ERROR!"
+      end
+
+    end # end else
+   
+  end # end charge.succeeded
 end
