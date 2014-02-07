93e8754d81d0dc5f08c640b3649c61b5fb59c6e9
Michael Blinn <michael.blinn@giv2giv.org>
2014-02-07 02:57:23 +0000

    Consolidating one-time and recurring processes into one easy one

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,10 +1,16 @@
 Stripe.api_key = App.stripe["api_key"]
 
-STRIPE_FEES = App.stripe["fees"]
-STRIPE_FEES_CENTS = App.stripe["fees_cents"]
 
 StripeEvent.setup do
   subscribe 'charge.succeeded' do |event|
     StripeCallbacks.new.charge_succeeded(event)
   end # end charge.succeeded
+
+  #events.all do |event|
+  #	StripeLog.new(event)
+    # Handle all event types - logging, etc.
+    #event.type        #=> "charge.failed"
+    #event.data.object #=> #<Stripe::Charge:0x3fcb34c115f8>
+  #end
+
 end
--- lib/stripe_callbacks.rb
+++ lib/stripe_callbacks.rb
@@ -1,70 +1,32 @@
 class StripeCallbacks
 
-  SHARE_PRECISION = App.giv["share_precision"]
+  def charge_succeeded(event)
 
-
-  def process_one_time_payment(event)
-    
-    transaction = Stripe::BalanceTransaction.retrieve(event.data.object.balance_transaction)
-    subscription = DonorSubscription.find_by_stripe_subscription_id(event.data.object.id) # stripe charge.id
-
-    charge_amount = BigDecimal(transaction.amount.to_s) / 100
-    transaction_fee = BigDecimal(transaction.fee.to_s) / 100
-    donation_amount = BigDecimal(transaction.net.to_s) / 100
-
-    donation = Donation.add_donation(subscription.id, charge_amount, transaction_fee, donation_amount)
-
-    if !donation.save
-      puts "ERROR!" # TODO: better error handling
-      return
-    end
-
-    donor = Donor.find(subscription.donor_id)
-    DonorMailer.charge_success(donor.email, charge_amount).deliver
-
-  end
-
-  def process_recurring_payment(event)
-
-    invoice = Stripe::Invoice.retrieve(event.data.object.invoice)
-    transaction = Stripe::BalanceTransaction.retrieve(event.data.object.balance_transaction)
-
-    charge_amount = BigDecimal(transaction.amount.to_s) / 100
-    transaction_fee = BigDecimal(transaction.fee.to_s) / 100
-    donation_amount = BigDecimal(transaction.net.to_s) / 100
-
-    subscription = DonorSubscription.find_by_stripe_subscription_id(invoice.lines.data.first.id) # stripe subscription.id
-
-    donation = Donation.add_donation(subscription.id, charge_amount, transaction_fee, donation_amount)
-
-    if !donation.save
-      puts "ERROR!" # TODO: better error handling
-      return
-    end
-
-    donor = Donor.find(subscription.donor_id)
-    DonorMailer.charge_success(donor.email, charge_amount).deliver
-
-  end
-
-
-	def charge_succeeded(event)
     invoice = event.data.object.invoice
-    
-    if invoice.blank?    
-      ret_invoice = nil
+
+    if invoice.blank?
+      subscription = DonorSubscription.find_by_stripe_subscription_id(event.data.object.id) # stripe charge.id
     else
-      ret_invoice = Stripe::Invoice.retrieve(invoice)
+      invoice = Stripe::Invoice.retrieve(event.data.object.invoice)
+      subscription = DonorSubscription.find_by_stripe_subscription_id(invoice.lines.data.first.id) # stripe subscription.id
     end
 
-    if ret_invoice.blank?
-      Rails.logger.debug "Processing one-time"
-    	process_one_time_payment(event)
+    transaction = Stripe::BalanceTransaction.retrieve(event.data.object.balance_transaction)
 
-    else
-      Rails.logger.debug "Processing recurring"
-    	process_recurring_payment(event)
-    end # end else
-   
-	end
+    charge_amount = BigDecimal(transaction.amount.to_s) / 100
+    transaction_fee = BigDecimal(transaction.fee.to_s) / 100
+    donation_amount = BigDecimal(transaction.net.to_s) / 100
+
+    donation = Donation.add_donation(subscription.id, charge_amount, transaction_fee, donation_amount)
+
+    if !donation.save
+      puts "ERROR!" # TODO: better error handling
+      return
+    end
+
+    donor = Donor.find(subscription.donor_id)
+    DonorMailer.charge_success(donor.email, charge_amount).deliver
+
+  end
+
 end
