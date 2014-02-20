9da2906ebb8ec8cf2845614a4c03a5c81b380ffc
Michael Blinn <michael.blinn@giv2giv.org>
2014-02-20 03:09:38 -0500

    Adding seeds for easy developer dive-in

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+
+# Set up a dev environment that will work
+if Rails.env.development?
+
+	Charity.create(name: "Test charity 1", ein: "Test EIN 1", city: "Anycity")
+	Charity.create(name: "Test charity 2", ein: "Test EIN 2", city: "Anycity")
+	Charity.create(name: "Test charity 3", ein: "Test EIN 3", city: "Anycity")
+	Charity.create(name: "Test charity 4", ein: "Test EIN 4", city: "Anycity")
+	Charity.create(name: "Test charity 5", ein: "Test EIN 5", city: "Anycity")
+
+	Share.create( stripe_balance: 0,
+								etrade_balance: 0,
+								share_total_beginning: 0,
+								shares_added_by_donation: 0,
+								shares_subtracted_by_grants: 0,
+    						share_total_end: 0, 
+    						donation_price: 100000.0,
+    						created_at: DateTime.now,
+    						updated_at: DateTime.now,
+    						grant_price: 100000.0)
+
+end
