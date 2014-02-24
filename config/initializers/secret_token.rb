dbb3815037248f446ba5cc385d16c6a0e1558b8e
Michael Blinn <michael.blinn@giv2giv.org>
2014-02-23 19:52:08 -0500

    cookie token to app.yml variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Giv2givRails::Application.config.secret_token = 'ffe79f5cbccdb1c9eeaa52ca9e90dd4ebff7367652919749cde859bd70d6235fab341ec69889b60fe1437eb1386f12edea5faaf5113e7750744073b72fd49542'
+Giv2givRails::Application.config.secret_token = App.giv["cookie_key"]
