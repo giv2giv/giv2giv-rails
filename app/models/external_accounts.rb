96a0d83e976a2f54a73ae4911f1fca71332b4f85
Michael Blinn <michael.blinn@giv2giv.org>
2014-03-30 15:43:23 +0000

    syntax and more work

--- app/models/external_accounts.rb
+++ app/models/external_accounts.rb
@@ -1,16 +1,14 @@
 class ExternalAccounts < ActiveRecord::Base
 
   belongs_to :donor
-
-  def self.create_with_omniauth(auth)
-  Rails.logger.debug 'hi'
+  def self.create_with_omniauth(auth, donor_id)
     create! do |account| # modify new account before it's saved, return new account
-      account.donor_id = current_donor.id
+      account.donor_id = donor_id
       account.provider = auth["provider"]
       account.uid = auth["uid"]
       account.name = auth["account_info"]["name"]
       account.oauth_token = auth["credentials"]["token"]
-      account.oauth_expires_at = Time.at(auth["credentials"]["expires_at"]
+      account.oauth_expires_at = Time.at(auth["credentials"]["expires_at"])
     end
   end
 
