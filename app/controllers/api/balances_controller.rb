810146bac9aa3893478e4ad4e0c7ab8cc10844c9
Michael Blinn <michael.blinn@giv2giv.org>
2016-01-06 00:29:15 +0000

    Sorting by amount, adding name

--- app/controllers/api/balances_controller.rb
+++ app/controllers/api/balances_controller.rb
@@ -9,10 +9,13 @@ class Api::BalancesController < Api::BaseController
     show_grants = grants.group(:charity_id).map do |grant|
       {
         'charity_id' => grant.charity_id,
+        'charity_name' => grant.charity.name,
         'charity_email' => grant.charity.email,
         'grant_amount' => grants.where("charity_id = ?", grant.charity_id).sum(:grant_amount) #TODO there must be a way to include sum in the mapped hash
       }
     end
+    show_grants = show_grants.sort_by { |hash| hash['name'].to_i }
+
     respond_to do |format|
       format.json { render json: show_grants }
     end
